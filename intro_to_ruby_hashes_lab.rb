4c8fa21db855b41b90526fcd344d6dba18ae656a
Matthew Dias Prada <matthew.dias.prada@gmail.com>
2019-08-20 12:32:33 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,10 +13,12 @@ end
 
 def id_generator
   # return a hash with a key :id assigned to positive integer
+  {:id => 4}
 end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
+  {key => value}
 end
 
 def read_from_hash(hash, key)
